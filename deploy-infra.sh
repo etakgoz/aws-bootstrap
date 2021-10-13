8b12b0e79c38d96955bc943db67ec2503034d645
etakgoz <tolga.akgoz@gmail.com>
2021-10-13 15:05:21 +0300

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,7 +4,22 @@ STACK_NAME=aws-bootstrap
 REGION=eu-central-1
 CLI_PROFILE=aws-bootstrap
 
-EC2_INSTANCE_TYPE=t2.medium
+EC2_INSTANCE_TYPE=t2.micro
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME-setup \
+    --template-file setup.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the Cloudfromation template
 echo -e "\n\n======= Deploying main.yml ========"
